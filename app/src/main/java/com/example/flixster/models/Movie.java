58b62fa2555fca94956a8916f052463dd52e2e20
Ali <1130aliz@gmail.com>
2021-01-30 21:53:19 -0500

    add detail activity

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,10 +3,12 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath;
     String title;
@@ -14,12 +16,19 @@ public class Movie {
 
     String backdropPath;
 
+    double rating;
+
+    //empty constructor needed by the Parceler Library
+    public Movie() {}
+
     public Movie(JSONObject jsonObject) throws JSONException {
         this.posterPath=jsonObject.getString("poster_path");
         this.title=jsonObject.getString("title");
         this.overview=jsonObject.getString("overview");
 
         this.backdropPath=jsonObject.getString("backdrop_path");
+
+        rating=jsonObject.getDouble("vote_average");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -44,5 +53,9 @@ public class Movie {
 
     public String getBackdropPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
